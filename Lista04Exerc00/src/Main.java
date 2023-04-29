6e62448b0c3dac79ce41d5c87b68c359f7992c70
Leonardo Castilho <79671553+CyborgLeunardu@users.noreply.github.com>
2023-04-28 21:18:49 -0300

    Exercício 00
    Exercício tabuada

---
+++ Lista04Exerc00/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 0;i <= 10; i++) {
+			System.out.printf("%d x %d = %d%n",i,n, i * n);
+		}
+		sc.close();
+
+	}
+
+}
